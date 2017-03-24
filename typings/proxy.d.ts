44000ec7eac8e26bfd982f85a932302438d829ef
Lostpigz <zjxszqc@live.com>
2017-03-24 17:33:47 +0900

    add typescript support
    add typescript support

---
+++ typings/proxy.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for proxy v0.2.4
+// Project: https://github.com/Lostpig/proxy
+// Definitions by: Lostpig <https://github.com/Lostpig/>
+
+import * as http from "http"
+import * as url from "url"
+
+declare namespace Proxy {
+    export declare interface IProxyOptions {
+        target: string | url.Url
+    }
+    export declare class ProxyServer extends http.Server {
+        on(event: 'proxyReq', listener: (proxyReq: http.ClientRequest, req: http.IncomingMessage, res: http.ServerResponse) => void): this
+        on(event: 'proxyRes', listener: (proxyRes: http.IncomingMessage, req: http.IncomingMessage, res: http.ServerResponse) => void): this
+        on(event: 'proxyEnd', listener: (req: http.IncomingMessage, res: http.ServerResponse, proxyRes: http.IncomingMessage) => void): this
+    }
+}
+
+declare function setup (server: http.Server, options: Proxy.IProxyOptions): Proxy.ProxyServer
+
+export = setup
